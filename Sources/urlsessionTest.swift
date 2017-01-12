d7bbb4d57baac775da644070609357fe204a9a40
Niklas Saers <niklas@saers.com>
2017-01-12 15:09:54 +0100

    Forgot this cleanup

--- Sources/urlsessionTest.swift
+++ Sources/urlsessionTest.swift
@@ -1,17 +1,16 @@
 import Foundation
-import XCTest
 #if os(Linux)
     import Dispatch
 #endif
 
-class urlsessionTest {
+public final class urlsessionTest {
 
     private let session: URLSession
     private let sessionConfiguration: URLSessionConfiguration
 
-    open var completionHandler : ((Data?, URLResponse?, Error?) -> Void)?
+    public var completionHandler : ((Data?, URLResponse?, Error?) -> Void)?
 
-    init() {
+    public init() {
 
         sessionConfiguration = URLSessionConfiguration.default
         sessionConfiguration.requestCachePolicy = NSURLRequest.CachePolicy.reloadIgnoringLocalCacheData
@@ -23,14 +22,14 @@ class urlsessionTest {
                              delegateQueue: OperationQueue())
     }
 
-    enum LocalError: Error {
+    public enum LocalError: Error {
         case cannotConvertUTF8ToData
         case cannotConvertStringToURL
         case completionHandlerMustBeSetBeforeCallingPost
     }
 
 
-    func testPost(username: String, password: String, url: String, postData: [String:Any]) throws {
+    public func testPost(username: String, password: String, url: String, postData: [String:Any]) throws {
 
         guard let url = URL(string: url) else {
             throw LocalError.cannotConvertStringToURL
@@ -57,7 +56,7 @@ class urlsessionTest {
 
     }
 
-    func basicAuthString(_ username: String, password: String) throws -> String {
+    private func basicAuthString(_ username: String, password: String) throws -> String {
 
         let loginString = "\(username):\(password)"
         guard let loginData: Data = loginString.data(using: .utf8) else {
